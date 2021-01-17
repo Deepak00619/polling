b5aada871daa55f07a6a1e1849193d3e11b77efe
Deepak kumar <deepu81629@.com>
2021-01-17 11:09:53 +0530

    fair

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://polling:polling@cluster0.nsnco.mongodb.net/polling?retryWrites=true&w=majority',
+    pusherAppId: '1139444',
+    pusherKey: '42f046855557f7a3392c',
+    pusherSecret: 'e032f3cf58e103dbdccb',
+    pusherCluster: 'ap2',
+    pusherEncrypted: true
+    };
+
+    // appId: "1139444",
+    // key: "42f046855557f7a3392c",
+    // secret: "e032f3cf58e103dbdccb",
+    // cluster: "ap2",
+    // useTLS: true
